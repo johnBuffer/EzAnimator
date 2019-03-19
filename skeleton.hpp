b2a7cbef67cd19237c512079ad829629e70e2328
Jean Tampon <jean.tampon@gmail.com>
2019-03-19 13:43:03 +0100

    Update skeleton.hpp

--- skeleton.hpp
+++ skeleton.hpp
@@ -3,8 +3,10 @@
 #include <vector>
 #include <memory>
 
+// Base class representing a 2D point
 class Point
 {
+public:
 	Point(const up::Vec2& position) :
 		m_position(position)
 	{}
@@ -13,14 +15,17 @@ class Point
 		m_position(x, y)
 	{}
 
+private:
+	// Radius is used for physics and GUI purpose
 	float m_radius;
 	up::Vec2 m_position;
 };
-
 using PointPtr = std::shared_ptr<Point>;
 
+// A connector is point to which it is possible to connect a bone
 class Connector
 {
+public:
 	Connector(const Point& position) :
 		m_point(position),
 		m_parent(nullptr)
@@ -31,24 +36,42 @@ class Connector
 		m_parent(parent)
 	{}
 
+public:
 	Point m_point;
 	PointPtr m_parent;
 
 	std::vector<PointPtr> m_children;
 };
-
 using ConnectorPtr = std::shared_ptr<Connector>;
 
+// A bone consist of a distance constraint between two connectors
 class Bone
 {
+public:
 	Bone(ConnectorPtr p1, ConnectorPtr p2):
 		m_p1(p1),
 		m_p2(p2)
 	{}
 
+private:
 	ConnectorPtr m_p1;
 	ConnectorPtr m_p2;
 };
-
 using BonePtr = std::shared_ptr<Bone>;
+
+// A skeleton is a hierachy of bones and connectors
+class Skeleton
+{
+public:
+	Skeleton() :
+		m_root({ 0.0f, 0.0f })
+	{}
+
+	void addBone(ConnectorPtr attach);
+
+private:
+	Connector m_root;
+	std::vector<ConnectorPtr> m_connectors;
+	std::vector<BonePtr> m_bones;
+};
 
